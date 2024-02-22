53c8ac541fc4676e1c6a1306cb72533f643087fb
itxshakil@gmail.com <itxshakil@gmail.com>
2024-02-22 16:23:06 +0530

    feat - Add multiple selection

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordion components are user interface elements used for organizing and presenting content in a collapsible manner."
+    },
+    {
+        id: '2',
+        question: "How do they work?",
+        answer: "Accordion components typically have multiple sections, each containing a question and an answer. When a user clicks on a question, the corresponding answer expands or collapses."
+    },
+    {
+        id: '3',
+        question: "What are the benefits of using accordion?",
+        answer: "Accordion components help in saving space on a webpage by hiding content until it's needed. They also provide a neat and organized way to present information."
+    },
+    {
+        id: '4',
+        question: "Are accordion components accessible?",
+        answer: "Yes, accordion components can be made accessible by ensuring proper keyboard navigation and providing ARIA attributes for screen readers."
+    },
+];
+
+export default data;
--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
+import data from "./data";
+import "./styles.css";
 
-export default function Accordian() {
-    return <div>Accordian</div>;
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    setMultiple((prevMultiple) => {
+      const index = prevMultiple.indexOf(getCurrentId);
+      if (index === -1) {
+        return [...prevMultiple, getCurrentId];
+      } else {
+        return prevMultiple.filter((id) => id !== getCurrentId);
+      }
+    });
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {enableMultiSelection
+          ? "Disable Multi Selection"
+          : "Enable Multi Selection"}
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content"> {dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found! </div>
+        )}
+      </div>
+    </div>
+  );
 }
